4c608a77239d24af3d2aec73451cb85eaf85da37
onezyhcn <166062831+onezyhcn@users.noreply.github.com>
2024-04-05 05:50:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,6 +13,7 @@ let addresses = [
 let addressesapi = [
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
 	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+	'https://wenben-6kg.pages.dev/IP.txt?token=123456', 
 ];
 
 // 设置优选地址，不带端口号默认80，noTLS订阅生成
